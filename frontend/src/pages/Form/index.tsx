0567c592ed4a061055e8162eea4e90a044e4bc69
Michel Alves <malvesdesign@hotmail.com>
2022-01-10 13:14:24 -0300

    Form layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,42 @@
+import './styles.css';
+
 function Form() {
+
+	const movie = {
+	    id: 1,
+	    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+	    title: "The Witcher",
+	    count: 2,
+	    score: 4.5
+	};
+
 	return(
-		<h1>Págian de formulário</h1>
+		<div className="mamovie-form-container">
+		    <img className="mamovie-movie-card-image" src={movie.image} alt={movie.title} />
+		    <div className="mamovie-card-bottom-container">
+		        <h3>{movie.title}</h3>
+		        <form className="mamovie-form">
+		            <div className="form-group mamovie-form-group">
+		                <label htmlFor="email">Informe seu email</label>
+		                <input type="email" className="form-control" id="email" />
+		            </div>
+		            <div className="form-group mamovie-form-group">
+		                <label htmlFor="score">Informe sua avaliação</label>
+		                <select className="form-control" id="score">
+		                    <option>1</option>
+		                    <option>2</option>
+		                    <option>3</option>
+		                    <option>4</option>
+		                    <option>5</option>
+		                </select>
+		            </div>
+		            <div className="mamovie-form-btn-container">
+		                <button type="submit" className="btn btn-primary mamovie-btn">Salvar</button>
+		            </div>
+		        </form >
+		        <button className="btn btn-primary mamovie-btn mt-3">Cancelar</button>
+		    </div >
+		</div >
 	);
 }
 
