a202bc07cfd5be116b20aa971b284d4deb6415d2
Michel Alves <malvesdesign@hotmail.com>
2022-01-10 14:06:38 -0300

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,18 @@
+import MovieStars from 'components/MovieStars';
+import './styles.css';
+
+function MovieScore() {
+
+	const score = 3.5;
+	const count = 13;
+	
+	return(
+		<div className="mamovie-score-container">
+		    <p className="mamovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+		    <MovieStars />
+		    <p className="mamovie-score-count">{count} avaliações</p>
+		</div>
+	);
+}
+
+export default MovieScore;
